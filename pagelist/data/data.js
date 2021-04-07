5f8f9cca2634bd84f0eb187f7ab3b7c695001537
Juyeon-0919 <60646121+Juyeon-0919@users.noreply.github.com>
2021-04-07 14:32:27 +0900

    init

---
+++ pagelist/data/data.js
@@ -0,0 +1,300 @@
+var data = {}
+data.tax2 = {
+    'title': '연말정산 TIP_21.01 개편',
+    'list': [
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_01_홈_02',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_01_홈_02'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_연말정산 지출리포트',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_연말정산 지출리포트'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_연말정산 지출 리포트 불러오기',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_연말정산 지출 리포트 불러오기'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_13월의 월급 계산법은',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_13월의 월급 계산법은'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_금융상품으로 세테크',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_금융상품으로 세테크'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_코로나 이후 2020연말정산 달라진 점은',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_코로나 이후 2020연말정산 달라진 점은'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_연말정산 지출 리포트 불러오기_조회 불가능 시간 팝업',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_연말정산 지출 리포트 불러오기_조회 불가능 시간 팝업'
+        }
+    ]
+};
+data.tax = {
+    'title': '연말정산 TIP 개편',
+    'list': [
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_01_홈_02',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_01_홈'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_01_홈_01_숨김',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_01_홈_01_숨김'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_01_홈_00_입력전',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_01_홈_00_입력전'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_01_홈_00_팝업_소득입력전',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_01_홈_00_팝업_소득입력전'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_02_소득정보',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_02_소득정보'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_03_편집',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_03_편집'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_04_상세정보_01_카드현금_01',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_04_상세정보_01_카드현금_01'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_04_상세정보_01_카드현금_02',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_04_상세정보_01_카드현금_02'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_04_상세정보_01_카드현금_03',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_04_상세정보_01_카드현금_03'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_04_상세정보_02_주택자금_01',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_04_상세정보_02_주택자금_01'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_04_상세정보_03_개인연금저축_01',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_04_상세정보_03_개인연금저축_01'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_04_상세정보_04_연금계좌_01',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_04_상세정보_04_연금계좌_01'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_04_상세정보_05_보험료_01',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_04_상세정보_05_보험료_01'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_04_상세정보_06_교육비',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_04_상세정보_06_교육비'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_04_상세정보_07_의료비',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_04_상세정보_07_의료비'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_04_상세정보_08_기부금',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_04_상세정보_08_기부금'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '06_연말정산TIP_06_상세정보가이드_00_연말정산가이드',
+            'category': '연말정산TIP',
+            'page': '06_연말정산TIP_06_상세정보가이드_00_연말정산가이드'
+        },
+    ]
+};
+data.event = {
+    'title': '이벤트',
+    'list': [
+        {
+            'stateClass': 'complete',
+            'url': '이벤트_아이엠스쿨',
+            'category': '이벤트',
+            'page': '이벤트_아이엠스쿨'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '이벤트_핑크다이어리',
+            'category': '이벤트',
+            'page': '이벤트_핑크다이어리'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '이벤트_응모',
+            'category': '이벤트',
+            'page': '이벤트_응모'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '이벤트팝업_01',
+            'category': '이벤트',
+            'page': '이벤트팝업_01'
+        },
+        {
+            'stateClass': 'complete',
+            'url': '이벤트팝업_02',
+            'category': '이벤트',
+            'page': '이벤트팝업_02'
+        },
+    ]
+};
+data.edit1 = {
+    'title': '2020.10.13 수정 요청 사항',
+    'list': [
+        {
+            'stateClass': 'complete',
+            'url': 'exependiture_03_account_detail_01',
+            'category': '지출관리',
+            'page': '지출관리_홈_계좌잔고_01_리스트'
+        },
+    ]
+};
+data.edit2 = {
+    'title': '2020.11.05 수정 요청 사항',
+    'list': [
+        {
+            'stateClass': 'complete',
+            'url': 'mycard_03_benefits_01',
+            'category': '지출관리',
+            'page': '05_내카드_03_카드혜택_01_가로형_01_기본'
+        },
+        {
+            'stateClass': 'complete',
+            'url': 'mycard_02_card_detail_01',
+            'category': '지출관리',
+            'page': '05_내카드_01_카드상세정보_01_가로형'
+        },
+        {
+            'stateClass': 'complete',
+            'url': 'mycard_01_connected',
+            'category': '지출관리',
+            'page': '05_내카드_00_홈_04_이전달'
+        },
+    ]
+};
+data.edit3 = {
+    'title': '2020.11.24 추가 요청 사항',
+    'list': [
+        {
+            'stateClass': 'complete',
+            'url': 'exependiture_01_account_list',
+            'category': '지출관리',
+            'page': '04_지출관리_00_홈_06_계좌상세_오픈뱅킹'
+        }
+    ]
+};
+data.edit4 = {
+    'title': '2020.12.07 추가 요청 사항',
+    'list': [
+        {
+            'stateClass': 'complete',
+            'url': 'sign_in_02',
+            'category': '약관',
+            'page': '약관'
+        },
+        {
+            'stateClass': 'complete',
+            'url': 'sign_in_03',
+            'category': '약관',
+            'page': '약관'
+        },
+        {
+            'stateClass': 'complete',
+            'url': 'sign_in_04',
+            'category': '약관',
+            'page': '약관'
+        },
+        {
+            'stateClass': 'complete',
+            'url': 'sign_in_05',
+            'category': '약관',
+            'page': '약관'
+        },
+        {
+            'stateClass': 'complete',
+            'url': 'sign_in_06',
+            'category': '약관',
+            'page': '약관'
+        },
+        {
+            'stateClass': 'complete',
+            'url': 'sign_in_02_content',
+            'category': '약관',
+            'page': '약관'
+        },
+        {
+            'stateClass': 'complete',
+            'url': 'sign_in_03_content',
+            'category': '약관',
+            'page': '약관'
+        },
+        {
+            'stateClass': 'complete',
+            'url': 'sign_in_04_content',
+            'category': '약관',
+            'page': '약관'
+        },
+        {
+            'stateClass': 'complete',
+            'url': 'sign_in_05_content',
+            'category': '약관',
+            'page': '약관'
+        },
+        {
+            'stateClass': 'complete',
+            'url': 'sign_in_06_content',
+            'category': '약관',
+            'page': '약관'
+        },
+    ]
+};
